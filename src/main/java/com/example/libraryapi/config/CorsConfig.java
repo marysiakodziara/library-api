c864c763aa04d000ed1a7c6602c085290d8a78fe
Marysia Antoniak <marysiaantoniak01@gmail.com>
2023-07-16 17:03:01 +0200

    Added cors configuration

---
+++ src/main/java/com/example/libraryapi/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.example.libraryapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("content-type", "authorization")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
+
--- src/main/java/com/example/libraryapi/service/BookService.java
+++ src/main/java/com/example/libraryapi/service/BookService.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Service;
 public class BookService {
 
     private final BookRepository bookRepository;
-    //private final BookMapper bookMapper;
 
     public List<BookDto> getAllBooks() {
 

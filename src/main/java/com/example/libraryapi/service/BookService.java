ad4bf04dd4f3929a9f6c7b4507fccb73f8a6a8e2
Marysia Antoniak <marysiaantoniak01@gmail.com>
2023-07-25 17:13:07 +0200

    Initial cache configuration with Caffeine

--- src/main/java/com/example/libraryapi/service/BookService.java
+++ src/main/java/com/example/libraryapi/service/BookService.java
@@ -7,6 +7,8 @@ import com.example.libraryapi.model.Book;
 import com.example.libraryapi.repository.BookRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +21,7 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
+    @Cacheable(value = "books_cache")
     public Page<BookDto> getAllBooks(int page, int size, String sortBy) {
         Pageable paging = PageRequest.of(page, size, Sort.by(sortBy));
         Page<Book> pagedResult = bookRepository.findAll(paging);
@@ -34,16 +37,19 @@ public class BookService {
         bookRepository.save(BookMapper.INSTANCE.map(bookDto));
     }
 
+    @Cacheable(value = "book_cache", key = "#id")
     public BookDto getBookById(Long id) {
         return BookMapper.INSTANCE.map(bookRepository.findById(id).orElse(null));
     }
 
+    @Cacheable(value = "books_by_category_cache")
     public Page<BookDto> getBooksByCategory(List<GenreEnum> categories, int page, int size, String sortBy) {
         Pageable paging = PageRequest.of(page, size, Sort.by(sortBy));
         Page<Book> pagedResult = bookRepository.findByCategoriesIn(categories, paging);
         return pagedResult.map(BookMapper.INSTANCE::map);
     }
 
+    @Cacheable(value = "books_by_phrase_cache")
     public Page<BookDto> getBooksContainingPhrase(String phrase, int page, int size, String sortBy) {
         Pageable paging = PageRequest.of(page, size, Sort.by(sortBy));
         Page<Book> pagedResult = bookRepository.findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(phrase, phrase, paging);
@@ -52,7 +58,12 @@ public class BookService {
 
     public Page<BookDto> getRandomBooks(int page, int size, String sortBy) {
         long count = bookRepository.count();
-        Page<Book> pagesResult = bookRepository.findByIdGreaterThanEqual((long) (Math.random() * count - 4), PageRequest.of(page, size, Sort.by(sortBy)));
+        return getByIdGreaterThanEqual((long) (Math.random() * count - 5), page, size, sortBy);
+    }
+
+    @Cacheable(value = "books_by_greater_id_cache", key = "#id")
+    public Page<BookDto> getByIdGreaterThanEqual(Long id, int page, int size, String sortBy) {
+        Page<Book> pagesResult = bookRepository.findByIdGreaterThanEqual(id, PageRequest.of(page, size, Sort.by(sortBy)));
         return pagesResult.map(BookMapper.INSTANCE::map);
     }
 }

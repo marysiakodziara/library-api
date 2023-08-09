541ba5ae170ec745cb4d49b6a64ed08fee602b2b
Marysia Antoniak <marysiaantoniak01@gmail.com>
2023-08-09 15:10:21 +0200

    Implemented scheduled task for canceling expired reservations

--- src/main/java/com/example/libraryapi/repository/ReservationRepository.java
+++ src/main/java/com/example/libraryapi/repository/ReservationRepository.java
@@ -2,11 +2,13 @@ package com.example.libraryapi.repository;
 
 import com.example.libraryapi.model.Reservation;
 import com.example.libraryapi.model.ReservationItem;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
     Page<Reservation> findByClient_EmailAddressAndBorrowed(String emailAddress, boolean borrowed, Pageable paging);
@@ -14,4 +16,13 @@ public interface ReservationRepository extends JpaRepository<Reservation, UUID> 
     List<Reservation> findByReservationItemsIn(List<ReservationItem> toList);
 
     Page<Reservation> findByCanceledFalseAndBorrowedFalse(Pageable paging);
+
+    @Query("""
+                SELECT r
+                FROM Reservation r
+                LEFT JOIN FETCH r.reservationItems
+                WHERE r.canceled = false
+                AND r.borrowed = false
+                AND r.endOfReservation < ?1""")
+    List<Reservation> findExpiredReservations(LocalDate today);
 }
